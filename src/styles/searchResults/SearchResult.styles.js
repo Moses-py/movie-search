b4edccc54ac7383cd61040a82195f40c6ac5aae1
Moses Chukwunekwu <dantereus1@gmail.com>
2022-12-17 13:36:17 +0100

    minor fix

--- src/styles/searchResults/SearchResult.styles.js
+++ src/styles/searchResults/SearchResult.styles.js
@@ -4,7 +4,7 @@ export const ResultContainer = styled.div`
   padding: 0 77px 48px 77px;
 
   @media (max-width: 600px) {
-    padding: 33px 28px 0 28px;
+    padding: 33px 28px 48px 28px;
   }
 `;
 
